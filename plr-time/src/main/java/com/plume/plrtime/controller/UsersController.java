929821c6a14ab2203693858060b17d5f93ffb088
grayplume <birdgray@163.com>
2025-03-26 23:03:22 +0800

    refactor(plr-time): 重构用户相关代码
    - 修改数据库连接 URL
    - 删除 User 相关的类和接口
    - 新增 Users 相关的控制器、服务和映射器
    - 更新 MyBatisX 模板配置- 添加 SQL 方言配置

---
+++ plr-time/src/main/java/com/plume/plrtime/controller/UsersController.java
@@ -0,0 +1,65 @@
+package com.plume.plrtime.controller;
+
+
+import com.plume.plrtime.common.Result;
+import com.plume.plrtime.pojo.Users;
+import com.plume.plrtime.service.UsersService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    private final UsersService usersService;
+
+    public UsersController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    /**
+     * 查询所有用户
+     */
+    @GetMapping("/list")
+    public Result list(){
+        List<Users> list = usersService.list();
+        return Result.success(list);
+    }
+
+    /**
+     * 添加用户
+     */
+    @PostMapping("/save")
+    public Result save(@RequestBody Users user){
+        boolean save = usersService.save(user);
+        return Result.success(save);
+    }
+
+    /**
+     * 登录
+     */
+    @PostMapping("/login")
+    public Result login(@RequestBody Users user){
+        Users login = usersService.login(user);
+        return Result.success(login);
+    }
+
+    /**
+     * 修改用户
+     */
+    @PostMapping("/update")
+    public Result update(@RequestBody Users user){
+        boolean update = usersService.updateById(user);
+        return Result.success(update);
+    }
+
+    /**
+     * 删除用户
+     */
+    @PostMapping("/delete/{ids}")
+    public Result delete(@PathVariable("ids") List<Long> ids){
+        boolean removed = usersService.removeBatchByIds(ids);
+        return Result.success(removed);
+    }
+}
