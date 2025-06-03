f0619a6de852aed36958652f867d67295cf8838c
grayplume <birdgray@163.com>
2025-06-03 15:49:35 +0800

    feat(statistics): 添加月度活动排行榜功能
    - 新增 /activityRankingByMonth 接口，用于获取指定月份的活动排行榜- 在 StatisticsService 中添加 selectMonthlyActivityRanking 方法实现月度活动排行逻辑
    - 修改前端页面，增加月度活动排行榜和占比饼图的展示
    - 优化页面布局，调整图表样式和交互

---
+++ plr-time/src/main/java/com/plume/plrtime/mapper/TodoListMapper.java
@@ -0,0 +1,18 @@
+package com.plume.plrtime.mapper;
+
+import com.plume.plrtime.pojo.TodoList;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author plume
+* @description 针对表【todo_list(任务表)】的数据库操作Mapper
+* @createDate 2025-06-03 11:15:28
+* @Entity com.plume.plrtime.pojo.TodoList
+*/
+public interface TodoListMapper extends BaseMapper<TodoList> {
+
+}
+
+
+
+
